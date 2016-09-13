e241999e67b05eaad06e10b89200aaa777f79681
Joao Santos <jfs.csantos@gmail.com>
2016-09-13 03:13:11 +0100

    Adding custom modals

--- app/scripts/portfolio-list/portfolio-list.js
+++ app/scripts/portfolio-list/portfolio-list.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('portfolioList', ['common.portfolio'])
+angular.module('portfolioList', ['common.portfolio', 'ngMaterial'])
   .component('portfolioList', {
     templateUrl: 'scripts/portfolio-list/portfolio-list.template.html',
-    controller: ['Portfolio', '$scope',
-      function PortfolioListController(Portfolio, $scope) {
+    controller: ['Portfolio', '$scope', '$mdDialog',
+      function PortfolioListController(Portfolio, $scope, $mdDialog) {
         $scope.portfolio = Portfolio.query();
         $scope.workId = 0;
 
@@ -12,6 +12,32 @@ angular.module('portfolioList', ['common.portfolio'])
           $scope.workId += 1;
           return $scope.workId;
         };
+
+        $scope.showDialog = function(ev, modalDiv) {
+          console.log('#' + modalDiv);
+          $mdDialog.show({
+            controller: DialogController,
+            contentElement: '#' + modalDiv,
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            fullscreen: true
+          });
+        };
+
+        function DialogController($scope, $mdDialog) {
+          $scope.hide = function() {
+            $mdDialog.hide();
+          };
+
+          $scope.cancel = function() {
+            $mdDialog.cancel();
+          };
+
+          $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+          };
+        }
     }
   ]
 });
