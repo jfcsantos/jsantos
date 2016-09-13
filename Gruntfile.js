38967611bf6f488d1d562a5f9291b03e32040d09
Joao Santos <jfs.csantos@gmail.com>
2016-09-13 17:39:26 +0100

    adding all html files to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -358,7 +358,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'views/{,*/}*.html',
+        src: ['*.html'],
         dest: '.tmp/templateCache.js'
       }
     },
